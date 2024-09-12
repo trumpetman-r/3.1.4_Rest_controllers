b9554614d5e2c0b1537159373135250edc5258ff
trumpetman-r <jmxtrumpet@gmail.com>
2024-09-12 21:52:41 +0300

    Done

--- src/main/java/com/webcrudsecurityboot/controller/UserController.java
+++ src/main/java/com/webcrudsecurityboot/controller/UserController.java
@@ -5,9 +5,9 @@ import com.webcrudsecurityboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
@@ -20,64 +20,10 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping
-    public List<User> getAllUsers() {
+    @GetMapping("/me")
+    public User getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentUsername = authentication.getName();
-        User currentUser = userService.getUserByEmail(currentUsername);
-        return List.of(currentUser);
-    }
-
-    @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String currentUsername = authentication.getName();
-        User currentUser = userService.getUserByEmail(currentUsername);
-
-        if (!currentUser.getId().equals(id)) {
-            throw new SecurityException("You are not authorized to view this user");
-        }
-
-        return currentUser;
-    }
-
-    @PostMapping
-    public void createUser(@RequestBody User user) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String currentUsername = authentication.getName();
-        User currentUser = userService.getUserByEmail(currentUsername);
-
-        if (!currentUser.getEmail().equals(user.getEmail())) {
-            throw new SecurityException("You are not authorized to create this user");
-        }
-
-        userService.saveUser(user);
-    }
-
-    @PutMapping("/{id}")
-    public void updateUser(@PathVariable Long id, @RequestBody User user) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String currentUsername = authentication.getName();
-        User currentUser = userService.getUserByEmail(currentUsername);
-
-        if (!currentUser.getId().equals(id)) {
-            throw new SecurityException("You are not authorized to update this user");
-        }
-
-        user.setId(id);
-        userService.updateUser(user);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String currentUsername = authentication.getName();
-        User currentUser = userService.getUserByEmail(currentUsername);
-
-        if (!currentUser.getId().equals(id)) {
-            throw new SecurityException("You are not authorized to delete this user");
-        }
-
-        userService.deleteUserById(id);
+        return userService.getUserByEmail(currentUsername);
     }
 }
